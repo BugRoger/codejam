a92b444c9d148d5d3db0b2e36dca2b666058e5c7
Michael Schmidt <michael02.schmidt@sap.com>
2011-06-02 04:50:35 +0200

    optimized it

--- lib/codejam/problems/CB.rb
+++ lib/codejam/problems/CB.rb
@@ -5,76 +5,33 @@ module CodeJam
   class CB < Problem 
 
     def prepare(input)
-      @boosters, @time, @planets, c, *@cycle = input[0].split.map(&:to_i)
-      
-      @path = []
-      @cycle.cycle((@planets.to_f / c).ceil) { |d| @path <<  d * 2 if @path.length < @planets}
+      @boosters, @build_time, @planets, @cycle_length, *@cycle = input[0].split.map(&:to_i)
     end 
       
     def solve
-      debug "Unboosted: #{@path}"
-      
-      boostable = boostable_path(@path)
-      # debug "Boostable: #{boostable}"
-      
-      savings   = savings_path(boostable)
-      debug "Savings:   #{savings}"
+      total_time      = 0
+      boostable       = false
+      boostable_edges = []
 
-      boosted   = subtract(@path, savings)
-      debug  "Boosted:   #{boosted}"
+      each_planet do |distance|
+        total_time += distance * 2
 
-      boosted.inject(0){|sum,x| sum + x } 
-    end
-
-
-    def subtract(a, b)
-      dummy = a.dup
-
-      b.each_with_index do |s, i|
-        dummy[i] -= s
-      end
-
-      dummy
-    end
-
-
-    def savings_path(path)
-      boostable = []
-      path.each_with_index { |p, i| boostable << [p,i] }
-
-      boostable.sort! { |a,b| b[0] - a[0] }
-
-      savings   = Array.new(path.length, 0) 
-      boostable.take(@boosters).each do |p|
-        dist, index = p
-        savings[index] = dist / 2
-      end
-
-      savings 
-    end
-
-    def boostable_path(path)
-      prio = path.dup 
-      unboosted = 0
-      
-      prio.each_with_index do |way, index|
-        if (unboosted + way) < @time
-          unboosted   += way
-          prio[index] = 0 
-        else
-          remainder = @time - unboosted
-          prio[index] -= remainder
-          break
+        unless boostable and total_time > @build_time
+          distance = (total_time - @build_time) / 2
+          boostable = true
         end
+
+        boostable_edges << distance 
       end
 
-
-      prio
+      total_time - boostable_edges.sort!.reverse!.first(@boosters).inject(&:+)
     end
 
-
-
+    def each_planet
+      0.upto(@planets - 1) do |i|
+        yield @cycle[i % @cycle_length]
+      end
+    end
 
   end
-
 end

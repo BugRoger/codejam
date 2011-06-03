2f5ae428c358eba26e6d8f16f4f3d554e55b3950
Michael Schmidt <michael02.schmidt@sap.com>
2011-06-03 14:08:19 +0200

    rename problems into contest

--- lib/codejam.rb
+++ lib/codejam.rb
@@ -3,4 +3,4 @@ require "codejam/runner"
 require "codejam/splitter"
 require "codejam/problem"
 
-Dir[File.dirname(__FILE__) + "/codejam/problems/*.rb"].each {|file| require file }
+Dir[File.dirname(__FILE__) + "/codejam/contests/**/*.rb"].each {|file| require file }

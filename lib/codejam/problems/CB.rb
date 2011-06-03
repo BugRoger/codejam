dfacf60ddb62ec3156c23b6586e439161ceec65e
Michael Schmidt <michael02.schmidt@sap.com>
2011-06-03 13:59:31 +0200

    debug and add test for small input

--- lib/codejam/problems/CB.rb
+++ lib/codejam/problems/CB.rb
@@ -5,32 +5,43 @@ module CodeJam
   class CB < Problem 
 
     def prepare(input)
-      @boosters, @build_time, @planets, @cycle_length, *@cycle = input[0].split.map(&:to_i)
+      @boosters, @build_time, @planets, @cycle_size, *@cycle = input[0].split.map(&:to_i)
     end 
       
     def solve
-      total_time      = 0
-      boostable       = false
-      boostable_edges = []
-
-      each_planet do |distance|
-        total_time += distance * 2
-
-        unless boostable and total_time > @build_time
-          distance = (total_time - @build_time) / 2
-          boostable = true
-        end
-
-        boostable_edges << distance 
-      end
-
-      total_time - boostable_edges.sort!.reverse!.first(@boosters).inject(&:+)
+      unboosted_time - boosted_time
     end
 
     def each_planet
       0.upto(@planets - 1) do |i|
-        yield @cycle[i % @cycle_length]
+        yield @cycle[i % @cycle_size]
       end
+    end
+      
+    def unboosted_time
+      total = 0
+      each_planet { |distance| total += distance }
+
+      total * 2
+    end
+
+    def boosted_time
+      total_time      = 0
+      building        = true
+      boostable_edges = [0]
+
+      each_planet do |distance|
+        total_time += distance * 2
+
+        boostable_edges << distance unless building
+
+        if building && total_time > @build_time
+          boostable_edges << (total_time - @build_time) / 2
+          building = false
+        end
+      end
+
+      boostable_edges.sort!{ |a, b| b <=> a }.first(@boosters).inject(&:+) or 0
     end
 
   end
